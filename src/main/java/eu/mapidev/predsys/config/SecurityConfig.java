7d6c8559335ee3494e2e0e64ac85fb1ef44d1fe8
Mark <mpietryga93@gmail.com>
2019-01-07 12:40:14 +0100

    Improve security config with some tests

--- src/main/java/eu/mapidev/predsys/config/SecurityConfig.java
+++ src/main/java/eu/mapidev/predsys/config/SecurityConfig.java
@@ -4,67 +4,41 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecurityConfig  {
-    
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     private final static Logger LOGGER = LoggerFactory.getLogger(SecurityConfig.class);
 
-    @Configuration
-    @Profile(Profiles.DEVELOPMENT)
-    public static class SecurityDevConfig extends WebSecurityConfigurerAdapter {
+    private final static String USERNAME = "admin";
+    private final static String SECRET = "{bcrypt}$2a$10$TO5q3d0bSzR.SbNMXaU6R.qV9/NG.Yh4xbx7WOLs.hux9BObYPDry";
 
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
-	    builder.inMemoryAuthentication()
-		    .withUser("admin").password("admin").roles("ADMIN").and();
-	}
-
-	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception {
-	    LOGGER.info("Run DEVELOPMENT Security Configuration");
-
-	    httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
-		    .authorizeRequests().antMatchers(HttpMethod.GET, "/draw").permitAll().and()
-		    .authorizeRequests().antMatchers("/static/**").permitAll().and()
-		    .authorizeRequests().antMatchers("/info/**").hasRole("ADMIN")
-		    .anyRequest().authenticated().and()
-		    .formLogin().loginPage("/login").permitAll().and()
-		    .httpBasic();
-	    httpSecurity.csrf().disable();
-	    httpSecurity.headers().frameOptions().disable();
-	}
-
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	auth.inMemoryAuthentication().passwordEncoder(encoder).withUser(USERNAME).password(SECRET).roles("ADMIN");
+	LOGGER.info("AUTHENTICATION USERNAME:" + USERNAME);
     }
 
-    @Configuration
-    @Profile(Profiles.PRODUCTION)
-    public static class SecurityProdConfig extends WebSecurityConfigurerAdapter {
-
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
-	    builder.inMemoryAuthentication()
-		    .withUser("marfik007").password("adidas007").roles("ADMIN").and();
-	}
-
-	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception {
-	    LOGGER.info("Run PRODUCTION Security Configuration");
-
-	    httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
-		    .authorizeRequests().antMatchers(HttpMethod.GET, "/draw").permitAll().and()
-		    .authorizeRequests().antMatchers("/static/**").permitAll().and()
-		    .authorizeRequests().antMatchers("/info/**").hasRole("ADMIN")
-		    .anyRequest().authenticated().and()
-		    .formLogin().loginPage("/login").permitAll().and()
-		    .httpBasic();
-	    httpSecurity.csrf().disable();
-	}
-
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+	httpSecurity.authorizeRequests()
+		.antMatchers("/profile").hasRole("ADMIN")
+		.antMatchers("/console/**").hasRole("ADMIN")
+		.antMatchers(HttpMethod.GET, "/draw/**").permitAll()
+		.antMatchers("/static/**").permitAll()
+		.antMatchers("/").permitAll()
+		.anyRequest().authenticated()
+		.and().httpBasic()
+		.and().csrf().ignoringAntMatchers("/console/**").ignoringAntMatchers("/draw/**")
+		.and().headers().frameOptions().sameOrigin();
     }
 }
