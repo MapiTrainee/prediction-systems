99bc849d87be4b46f6bb091fd23130f8fc116c01
Mark <mpietryga93@gmail.com>
2019-01-07 10:12:47 +0100

    Remove unnecessary files, classes

--- src/main/java/xyz/pietryga/predsys/Application.java
+++ src/main/java/xyz/pietryga/predsys/Application.java
@@ -5,13 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
+@SpringBootApplication
 public class Application {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
-
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-
+	SpringApplication.run(Application.class, args);
     }
 }
